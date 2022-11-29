8f8afcc56d80ace5a6630231fbf75f08c5953842
yuliepalechor <sanyulie08@gmail.com>
2022-11-28 20:23:46 -0500

    creacion de modal

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -22,7 +22,9 @@
 
           <template slot="footer">
             <div class="stats">
-              <md-icon>access_time</md-icon>
+              <md-button @click="show=true" class="md-icon-button" disabled>
+        <md-icon>add</md-icon>
+      </md-button>
               updated 4 minutes ago
             </div>
           </template>
@@ -282,6 +284,90 @@
         </nav-tabs-card> -->
       </div>
     </div>
+    ///inicializando modal
+    <div>
+    
+    <b-modal
+      v-model="show"
+      title="Modal Variants"
+      :header-bg-variant="headerBgVariant"
+      :header-text-variant="headerTextVariant"
+      :body-bg-variant="bodyBgVariant"
+      :body-text-variant="bodyTextVariant"
+      :footer-bg-variant="footerBgVariant"
+      :footer-text-variant="footerTextVariant"
+    >
+      <b-container fluid>
+        <b-row class="mb-1 text-center">
+          <b-col cols="3"></b-col>
+          <b-col>Background</b-col>
+          <b-col>Text</b-col>
+        </b-row>
+
+        <b-row class="mb-1">
+          <b-col cols="3">Header</b-col>
+          <b-col>
+            <b-form-select
+              v-model="headerBgVariant"
+              :options="variants"
+            ></b-form-select>
+          </b-col>
+          <b-col>
+            <b-form-select
+              v-model="headerTextVariant"
+              :options="variants"
+            ></b-form-select>
+          </b-col>
+        </b-row>
+
+        <b-row class="mb-1">
+          <b-col cols="3">Body</b-col>
+          <b-col>
+            <b-form-select
+              v-model="bodyBgVariant"
+              :options="variants"
+            ></b-form-select>
+          </b-col>
+          <b-col>
+            <b-form-select
+              v-model="bodyTextVariant"
+              :options="variants"
+            ></b-form-select>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col cols="3">Footer</b-col>
+          <b-col>
+            <b-form-select
+              v-model="footerBgVariant"
+              :options="variants"
+            ></b-form-select>
+          </b-col>
+          <b-col>
+            <b-form-select
+              v-model="footerTextVariant"
+              :options="variants"
+            ></b-form-select>
+          </b-col>
+        </b-row>
+      </b-container>
+
+      <template #modal-footer>
+        <div class="w-100">
+          <p class="float-left">Modal Footer Content</p>
+          <b-button
+            variant="primary"
+            size="sm"
+            class="float-right"
+            @click="show=false"
+          >
+            Close
+          </b-button>
+        </div>
+      </template>
+    </b-modal>
+  </div>
   </div>
 </template>
 
@@ -304,6 +390,15 @@ export default {
   },
   data() {
     return {
+      show: false,
+        variants: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'],
+        headerBgVariant: 'dark',
+        headerTextVariant: 'light',
+        bodyBgVariant: 'light',
+        bodyTextVariant: 'dark',
+        footerBgVariant: 'warning',
+        footerTextVariant: 'dark',
+
       dailySalesChart: {
         data: {
           labels: ["M", "T", "W", "T", "F", "S", "S"],
