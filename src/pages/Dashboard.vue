b2fdf896cd6a2db5b8553255e0dc55c685981bd7
alejandropr3 <alejandro.perez66@misena.edu.co>
2022-12-15 20:49:43 -0500

    actualizacion listar por fecha eventos y noticias y incidencias

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -38,7 +38,7 @@
             <div class="stats">
               <md-icon>access_time</md-icon>
               actualizado hace 4 dias
-              <md-button @click="show = true" class="md-icon-button md-raised md-primary">
+              <md-button @click="showincidencia= true" class="md-icon-button md-raised md-primary">
                 <md-icon>menu</md-icon>
               </md-button>
             </div>
@@ -234,6 +234,127 @@
 
 
 
+
+
+
+
+
+
+
+ <!-- primer modal  prueba incidencias-->
+
+        <b-modal v-model="showincidencia" title="Listar por fecha" :header-bg-variant="headerBgVariant"
+        :header-text-variant="headerTextVariant" :body-bg-variant="bodyBgVariant" :body-text-variant="bodyTextVariant"
+        :footer-bg-variant="footerBgVariant" :footer-text-variant="footerTextVariant">
+        <b-container fluid>
+
+          <!-- fechaeventos input inicio -->
+          <div>
+            <label for="example-input">Filtrar fecha</label>
+            <b-input-group class="mb-3">
+              <b-form-input id="fechaEvento" name="fechita" v-model="value" type="text" placeholder="YYYY-MM-DD" autocomplete="off"></b-form-input>
+              <b-input-group-append>
+                <b-form-datepicker v-model="value" button-only right locale="en-US" aria-controls="example-input"
+                  @context="onContext"></b-form-datepicker>
+              </b-input-group-append>
+
+            </b-input-group>
+            
+            <!-- <p class="mb-1">Value: '{{ value }}'</p>
+            <p class="mb-1">Selected: '{{ selected }}'</p>
+            <p>Formatted: '{{ formatted }}'</p> -->
+          </div>
+          <!-- fecha input final -->
+
+
+
+          <!-- incidencias lista -->
+          <!-- <div>
+                  <b-table :filter="filter" id:="tablaIncidencias" :per-page="perpage" :current-page="currentPage" striped
+                hover responsive class="mt-4" :fields="encabezado2" :items="incidencias">
+
+                <template v-slot:cell(Ver)="row">
+                    <b-button v-b-modal.modal-scrollable @click="VerIncidencia(row.item.id)"
+                        class="md-just-icon md-simple md-primary">
+                        <md-icon>edit</md-icon>
+                        <md-tooltip md-direction="top">Ver</md-tooltip>
+                    </b-button>
+                </template>
+
+            </b-table>
+                </div> -->
+          
+
+          <b-button v-b-modal="" @click="getincidenciafecha(value)" variant="primary" size="sm"
+            class="float-right">Listar
+          </b-button>
+
+        </b-container>
+
+        <template #modal-footer>
+          <div class="w-100">
+            <p class="float-left"></p>
+            <b-button variant="primary" size="sm" class="float-right" @click="showincidencia = false">cerrar
+            </b-button>
+          </div>
+
+          <!--       lista eventos por fecha     abre segundo modal       -->
+          <div>
+            <b-modal size="xl" id="asignacionnueva" title="incidencias por fecha"  v-model="showTablaincidenciafecha">
+
+              <b-container fluid>
+                <div>
+                  <b-table :filter="filter" id:="tablaIncidencias" :per-page="perpage" :current-page="currentPage" striped
+                hover responsive class="mt-4" :fields="encabezado2" :items="incidencias">
+
+                <template v-slot:cell(Ver)="row">
+                    <b-button v-b-modal.modal-scrollable @click="VerIncidencia(row.item.id)"
+                        class="md-just-icon md-simple md-primary">
+                        <md-icon>edit</md-icon>
+                        <md-tooltip md-direction="top">Ver</md-tooltip>
+                    </b-button>
+                </template>
+
+            </b-table>
+                </div>
+              </b-container>
+              <template #modal-footer>
+                <div class="w-100">
+
+                </div>
+              </template>
+
+              <template>
+
+              </template>
+            </b-modal>
+
+          </div>
+          <!-- final modal de eventos por fecha lista -->
+
+          <!--       lista de noticias x fecha segundo modal       -->
+          
+
+        </template>
+      </b-modal>
+
+
+      <!-- final primermodal prueba  incidencias-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
         <md-card>
 
@@ -279,6 +400,26 @@ import {
 
 } from "@/components";
 import { async } from "q";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue, get, child, set, update, remove, equalTo, push } from "firebase/database";
+
+
+//configurar conexion proyecto con firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDeULle7wFJIoWjafOyoGEG3u4_P2e9zEU",
+    authDomain: "androidtodes.firebaseapp.com",
+    databaseURL: "https://androidtodes-default-rtdb.firebaseio.com",
+    projectId: "androidtodes",
+    storageBucket: "androidtodes.appspot.com",
+    messagingSenderId: "390356232637",
+    appId: "1:390356232637:web:8d1bee1df43c2378bdcad4",
+    measurementId: "G-V8Y63159Y9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 
 export default {
   components: {
@@ -291,12 +432,13 @@ export default {
       showTablaEventofecha: false,
     
       showTablaNoticiafecha:false,
+      showTablaincidenciafecha:false,
 
-      fecha:{
-        fechita:null
-      },
+
+      showincidencia:false,
       show1: false,
       publicacion: [],
+      incidencias: [],
       
       variants: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'],
       headerBgVariant: 'dark',
@@ -409,28 +551,20 @@ export default {
         { key: "fecha_caducidad", label: "fecha-caducidad" },
         { key: "tipo", label: "Tipo" },
       ],
+      encabezado2: [
+                //{ key: "id", label: "id" },
+                { key: "estado", label: "Estado" },
+                { key: "date", label: "Fecha" },
+                { key: "descripcion", label: "Descripcion" },
+                { key: "barrio", label: "Barrio" },
+
+                { key: "Ver", label: "Ver" }
+            ],
 
     };
   },
 
   methods: {
-    getevento() {
-      this.axios
-        .get("http://127.0.0.1:8000/api/eventos")
-        .then((response) => {
-          this.eventos = response.data;
-        });
-    },
-
-    //noticias por fecha
-    getnoticia() {
-      this.axios
-        .get("http://127.0.0.1:8000/api/noticias")
-        .then((response) => {
-         this.noticias = response.data;
-        });
-    },
-
     // trae noticias por fecha button
     geteventofecha(fecha) {
       this.axios
@@ -453,6 +587,41 @@ export default {
         });
         
     },
+    getincidenciafecha(fecha) {
+
+      const dbIncidenciasRef2 = ref(database, 'Incidencias/')
+            onValue(dbIncidenciasRef2, (snapshot) => {
+                snapshot.forEach((childSnapshot) => {
+                    const childKey = childSnapshot.key;
+                    const childData = childSnapshot.val();
+
+                    console.log(childData)
+                    if (fecha == childSnapshot.val().date) {
+                        this.incidencias.push({
+                            id: childKey,
+                            IdUsuario: childSnapshot.val().IdUsuario,
+                            barrio: childSnapshot.val().barrio,
+                            date: childSnapshot.val().date,
+                            descripcion: childSnapshot.val().descripcion,
+                            edad: childSnapshot.val().edad,
+                            estado: childSnapshot.val().estado,
+                            hora: childSnapshot.val().hora,
+                            nombres: childSnapshot.val().nombres,
+                            ubicacion: childSnapshot.val().ubicacion,
+                            urlimagen: childSnapshot.val().urlimagen,
+                        })
+
+                    }
+                    
+                });
+               
+            }, {
+               
+                onlyOnce: true
+            });
+            this.showTablaincidenciafecha=true;
+        
+    },
 
     //datepicker
     onContext(ctx) {
@@ -461,6 +630,7 @@ export default {
       // The following will be an empty string until a valid date is entered
       this.selected = ctx.selectedYMD
     }
+
   },
   mounted() {
     //this.getevento();
